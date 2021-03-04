7760ff4ecca58b1cd4ef1c8bcf8ae4d2936209a3
Tim Ward <timward@nextvision.biz>
2021-03-04 08:23:24 +0000

    6/ Organise the UI into 2 areas - a button area and a field

--- src/root.tsx
+++ src/root.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
  
-export const Root = () => {
+export const Root = (): JSX.Element => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
      
       useEffect(() => {
@@ -11,15 +11,24 @@ export const Root = () => {
           const context = canvas.getContext('2d');
           if (context !== null) {
             context.fillStyle = 'green';
-            context.fillRect(20, 10, 200, 100);
+            context.fillRect(20, 10, 300, 300);
           }
         }
       })
        
-       return (
-           <canvas
+       return <div className="total"
+         style={{ width: '400px', height: '400px', display: "flex", flexDirection: "row"}}
+       > 
+        <div className="buttonsArea"
+             style={{ width: '200px', height: '200px'}}
+        >
+          <h1>Buttons Area</h1>
+        </div>
+        <div className="field">
+         <canvas 
                ref={canvasRef} 
-               style={{ width: '100px', height: '100px' }}
-           />
-       );
+//               style={{ width: '200px', height: '200px' }}
+              />
+        </div>
+      </div>
     }
