32d5f7aba0e490bdfeaeef908e87bbae343e9d75
Tim Ward <timward@nextvision.biz>
2021-03-04 07:16:52 +0000

    4/ Convert to the React Hooks style

--- src/root.tsx
+++ src/root.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
-export class Root extends React.Component {
-    render() {
-        return (
-            <h1>Hello World</h1>
-        );
-    }
+export const Root = () => {
+  return (
+      <h1>Hello using React Hooks</h1>
+  )
 }
