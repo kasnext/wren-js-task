f1e5090e1412e01a73b8409e918a776f2503b34f
Tim Ward <timward@nextvision.biz>
2021-03-04 07:37:17 +0000

    5/ Use the Canvas API to draw a green rectangle

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
-
+import { useEffect } from 'react';
+import { useRef } from 'react';
+ 
 export const Root = () => {
-  return (
-      <h1>Hello using React Hooks</h1>
-  )
-}
+    const canvasRef = useRef<HTMLCanvasElement>(null)
+     
+      useEffect(() => {
+          const canvas = canvasRef.current;
+          if (canvas !== null) {
+          const context = canvas.getContext('2d');
+          if (context !== null) {
+            context.fillStyle = 'green';
+            context.fillRect(20, 10, 200, 100);
+          }
+        }
+      })
+       
+       return (
+           <canvas
+               ref={canvasRef} 
+               style={{ width: '100px', height: '100px' }}
+           />
+       );
+    }
