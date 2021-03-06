c02114e0a877daa6220816050a881b184e4d4e93
Tim Ward <timward@nextvision.biz>
2021-03-06 08:45:56 +0000

    15/ Create an array of sheep and test

--- src/sheep.test.ts
+++ src/sheep.test.ts
@@ -5,10 +5,24 @@ import { pipe } from "fp-ts/lib/function"
 test.skip ('SanityTest', () => expect (true).toBe (true) )
 
 test.only ('createSheep', () => testCreateSheep )
+test.only ('createArrayOfSheep', () => createArrayOfSheep )
+
+//--------------------------------------------------------
+
+const createFlossy = createSheep (0, 'Flossy', false, {x: 0, y: 0})
+const createFred = createSheep (0, 'Fred', true, {x: 100, y: 100})
+
+
 
 const testCreateSheep: void =
   pipe (
-    createSheep (0, 'flossy', true, {x: 0, y: 0}),
-    sheep => expect (sheep.name).toBe ('flossy'),
+    createFlossy,
+    sheep => expect (sheep.name).toBe ('Flossy'),
+  )
+
+const createArrayOfSheep: void =
+  pipe (
+    [createFlossy, createFred],
+    sheepArray => expect (sheepArray.length).toBe (2),
   )
 
