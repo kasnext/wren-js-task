221d6cc587f2826e099fd1ec0d0d41f3d8995991
Tim Ward <timward@nextvision.biz>
2021-03-06 11:44:06 +0000

    16/ Add a test to update the position of one sheep in an array of sheep
    npm install immer

--- src/sheep.test.ts
+++ src/sheep.test.ts
@@ -1,16 +1,31 @@
-import { createSheep } from "./sheep"
+import { createSheep, ISheep } from "./sheep"
+
+// 3rd party imports
+import produce from 'immer';
 import { pipe } from "fp-ts/lib/function"
 
 
 test.skip ('SanityTest', () => expect (true).toBe (true) )
 
-test.only ('createSheep', () => testCreateSheep )
-test.only ('createArrayOfSheep', () => createArrayOfSheep )
+test.skip ('createSheep', () => testCreateSheep )
+test.skip ('createArrayOfSheep', () => testCreateArrayOfSheep )
+test.only ('testUpdateArrayOfSheep', () => testUpdateArrayOfSheep )
 
 //--------------------------------------------------------
 
 const createFlossy = createSheep (0, 'Flossy', false, {x: 0, y: 0})
 const createFred = createSheep (0, 'Fred', true, {x: 100, y: 100})
+
+const createArrayOfSheep: ISheep[] =
+  [createFlossy, createFred]
+
+const updateSheepPos: (sheep: ISheep) => ISheep =
+(sheep: ISheep) =>
+  produce (sheep, draft => {draft.point.x = sheep.point.x + 1})      
+
+const updateArrayOfSheep: (sheep: ISheep[]) => ISheep[] =
+(sheep: ISheep[]) => 
+  sheep.map ( sheepX => updateSheepPos (sheepX))
 
 
 
@@ -20,9 +35,19 @@ const testCreateSheep: void =
     sheep => expect (sheep.name).toBe ('Flossy'),
   )
 
-const createArrayOfSheep: void =
+const testCreateArrayOfSheep: void =
   pipe (
-    [createFlossy, createFred],
+    createArrayOfSheep,
     sheepArray => expect (sheepArray.length).toBe (2),
+  )
+
+const testUpdateArrayOfSheep: void =
+  pipe (
+    createArrayOfSheep,
+    updateArrayOfSheep,
+    sheepArray => {
+      expect (sheepArray.length).toBe (2),
+      expect (sheepArray[0].point.x).toBe (1)
+    }
   )
 
